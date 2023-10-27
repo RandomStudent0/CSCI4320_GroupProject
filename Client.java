b4958946a7f1d4f43d5fb471fbf9eb26a312ccb4
anguyen36clayton <anguyen36@student.clayton.edu>
2023-10-26 21:47:55 -0400

    Moved client to a different branch

--- Client.java
+++
@@ -1,8 +0,0 @@
-package AbstractFactory;
-
-public class Client {
-    public static void main (String[] args) {
-        System.out.println("Test");
-        Login.getCredentials(); //Asks user to input login info
-    }
-}
