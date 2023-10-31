a1fb7c297e447f44783de6efbb2ce46bd472da24
anguyen36clayton <anguyen36@student.clayton.edu>
2023-10-31 16:18:32 -0400

    Replaced login in page name

--- Client.java
+++ Client.java
@@ -3,6 +3,7 @@ package CSCI4320GroupProject;
 public class Client {
     public static void main (String[] args) {
         System.out.println("Test");
-        Login.getCredentials(); //Asks user to input login info
+        //Login.getCredentials(); //Asks user to input login info
+        Login.firstPage();
     }
 }
