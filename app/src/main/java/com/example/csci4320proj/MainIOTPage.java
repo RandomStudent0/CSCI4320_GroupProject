c9d6f3558979cda3fb114ce28d6599c561d46ea8
anguyen36clayton <anguyen36@student.clayton.edu>
2023-11-10 13:48:07 -0500

    Added a template for activities

--- app/src/main/java/com/example/csci4320proj/MainIOTPage.java
+++ app/src/main/java/com/example/csci4320proj/MainIOTPage.java
@@ -1,4 +1,6 @@
+/*
 package com.example.csci4320proj;
+
 
 import android.os.Bundle;
 
@@ -13,4 +15,20 @@ public class MainIOTPage extends AppCompatActivity {
     }
 }
 
-//SAVE THIS AS A TEMPLATE LATER FOR NEW PAGES
+*/
+//The above is the template for a new page. Do not delete it until we turn the project in.
+
+package com.example.csci4320proj;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainIOTPage extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_iot_page); // Set the new layout for this activity
+        // Additional setup for the new activity
+    }
+}
