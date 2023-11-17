2e421db6209ee3cba7ba9633f16e3353f4d4b533
anguyen36clayton <anguyen36@student.clayton.edu>
2023-11-16 19:21:09 -0500

    Added functional Robot Vacuum IOT

--- app/src/main/java/com/example/csci4320proj/RobotVacMain.java
+++ app/src/main/java/com/example/csci4320proj/RobotVacMain.java
@@ -1,5 +1,6 @@
 package com.example.csci4320proj;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,105 +11,202 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RobotVacMain extends AppCompatActivity {
-    private Switch sw1;
-    private Switch sw2;
-    private Switch sw3;
-    private Switch sw4;
-    private TextView text1;
-    private TextView text2;
+    private Button chargeButton;
+    private TextView chargeText;
+
+    private Button room1Button;
+
+    private Button room2Button;
+    private Button room3Button;
+
+
+
+    private TextView room1Text;
+    private TextView room2Text;
+    private TextView room3Text;
+
+    private TextView suctionQuiet;
+    private TextView suctionMedium;
+    private TextView suctionMaximum;
+    private Button quietButton;
+    private Button mediumButton;
+    private Button maximumButton;
+
+    private Button backButton;
+
+    private Button offButton;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.testrobotvac); // Set the new layout for this activity
+        setContentView(R.layout.robot_vac); // Set the new layout for this activity
 
-        sw1 = findViewById(R.id.sw1);
-        sw2 = findViewById(R.id.sw2);
-        sw3 = findViewById(R.id.sw3);
-        sw4 = findViewById(R.id.sw4);
-        text1 = findViewById(R.id.text1);
-        text2 = findViewById(R.id.text2);
+        chargeButton = findViewById(R.id.chargeButton);
+        chargeText = findViewById(R.id.chargeText);
 
-        //if charge switch is on, then other switches off and charge is visible
-        sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        room1Button = findViewById(R.id.room1Button);
+
+        room2Button = findViewById(R.id.room2Button);
+        room3Button = findViewById(R.id.room3Button);
+
+
+        room1Text = findViewById(R.id.room1Text);
+        room2Text = findViewById(R.id.room2Text);
+        room3Text = findViewById(R.id.room3Text);
+
+        quietButton = findViewById(R.id.quietButton);
+        mediumButton = findViewById(R.id.mediumButton);
+        maximumButton = findViewById(R.id.maximumButton);
+
+        suctionQuiet = findViewById(R.id.suctionQuiet);
+        suctionMedium = findViewById(R.id.suctionMedium);
+        suctionMaximum = findViewById(R.id.suctionMaximum);
+
+        offButton = findViewById(R.id.offButton);
+
+        backButton = findViewById(R.id.backButton);
+
+
+        chargeButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //action with switch on/off
-                if(b) {
-                    //other switches false
-                    text1.setVisibility(View.VISIBLE);
-                    text2.setVisibility(View.INVISIBLE);
-                    sw2.setChecked(false);
-                    sw3.setChecked(false);
-                    sw4.setChecked(false);
+            public void onClick(View view) {
+                if (chargeText.getVisibility() == View.INVISIBLE) {
+                    chargeText.setVisibility(View.VISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 } else {
-                    //if off
-                    text1.setVisibility(View.INVISIBLE);
-                    text2.setVisibility(View.INVISIBLE);
-                }
-            }
-            });
-
-        //if room 1 switch is on, then other switches off and cleaning is visible
-        sw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //action with switch on/off
-                if(b) {
-                    //other switches false
-                    text2.setVisibility(View.VISIBLE);
-                    sw1.setChecked(false);
-                    sw3.setChecked(false);
-                    sw4.setChecked(false);
-                } else {
-                    //if off
-                    text2.setVisibility(View.INVISIBLE);
-                    text1.setVisibility(View.INVISIBLE);
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        //if room 2 switch is on, then other switches off and cleaning is visible
-        sw3.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        room1Button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //action with switch on/off
-                if(b) {
-                    //other switches false
-                    text2.setVisibility(View.VISIBLE);
-                    sw1.setChecked(false);
-                    sw2.setChecked(false);
-                    sw4.setChecked(false);
+            public void onClick(View view) {
+                if (room1Text.getVisibility() == View.INVISIBLE) {
+                    room1Text.setVisibility(View.VISIBLE);
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 } else {
-                    //if off
-                    text1.setVisibility(View.INVISIBLE);
-                    text2.setVisibility(View.INVISIBLE);
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        //if room 3 switch is on, then other switches off and cleaning is visible
-        sw4.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        room2Button.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                //action with switch on/off
-                if(b) {
-                    //other switches false
-                    text2.setVisibility(View.VISIBLE);
-                    sw1.setChecked(false);
-                    sw2.setChecked(false);
-                    sw3.setChecked(false);
+            public void onClick(View view) {
+                if (room2Text.getVisibility() == View.INVISIBLE) {
+                    room2Text.setVisibility(View.VISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 } else {
-                    //if off
-                    text1.setVisibility(View.INVISIBLE);
-                    text2.setVisibility(View.INVISIBLE);
-
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
                 }
             }
         });
 
-        // Additional setup for the new activity
+        room3Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (room3Text.getVisibility() == View.INVISIBLE) {
+                    room3Text.setVisibility(View.VISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    chargeText.setVisibility(View.INVISIBLE);
+                } else {
+                    chargeText.setVisibility(View.INVISIBLE);
+                    room1Text.setVisibility(View.INVISIBLE);
+                    room2Text.setVisibility(View.INVISIBLE);
+                    room3Text.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        quietButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (suctionQuiet.getVisibility() == View.INVISIBLE) {
+                    suctionQuiet.setVisibility(View.VISIBLE);
+                    suctionMedium.setVisibility(View.INVISIBLE);
+                    suctionMaximum.setVisibility(View.INVISIBLE);
+                } else {
+                    suctionQuiet.setVisibility(View.INVISIBLE);
+                    suctionMedium.setVisibility(View.INVISIBLE);
+                    suctionMaximum.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        mediumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (suctionMedium.getVisibility() == View.INVISIBLE) {
+                    suctionMedium.setVisibility(View.VISIBLE);
+                    suctionQuiet.setVisibility(View.INVISIBLE);
+                    suctionMaximum.setVisibility(View.INVISIBLE);
+                } else {
+                    suctionQuiet.setVisibility(View.INVISIBLE);
+                    suctionMedium.setVisibility(View.INVISIBLE);
+                    suctionMaximum.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        maximumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (suctionMaximum.getVisibility() == View.INVISIBLE) {
+                    suctionMaximum.setVisibility(View.VISIBLE);
+                    suctionQuiet.setVisibility(View.INVISIBLE);
+                    suctionMedium.setVisibility(View.INVISIBLE);
+                } else {
+                    suctionQuiet.setVisibility(View.INVISIBLE);
+                    suctionMedium.setVisibility(View.INVISIBLE);
+                    suctionMaximum.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        offButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                suctionMaximum.setVisibility(View.INVISIBLE);
+                suctionQuiet.setVisibility(View.INVISIBLE);
+                suctionMedium.setVisibility(View.INVISIBLE);
+                room1Text.setVisibility(View.INVISIBLE);
+                room2Text.setVisibility(View.INVISIBLE);
+                room3Text.setVisibility(View.INVISIBLE);
+                chargeText.setVisibility(View.INVISIBLE);
+            }
+        });
+
+
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RobotVacMain.this, MainIOTPage.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
 
-//The above is the template for a new page. Do not delete it until we turn the project in.
