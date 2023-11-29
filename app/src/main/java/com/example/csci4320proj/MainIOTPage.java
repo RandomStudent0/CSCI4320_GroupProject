badb098e2ba38231f46fdb44108f6697ed1d5bd2
anguyen36clayton <anguyen36@student.clayton.edu>
2023-11-28 23:21:56 -0500

    Removed unused imports and useless comments.

--- app/src/main/java/com/example/csci4320proj/MainIOTPage.java
+++ app/src/main/java/com/example/csci4320proj/MainIOTPage.java
@@ -1,20 +1,3 @@
-/*
-package com.example.csci4320proj;
-
-import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainIOTPage extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_iot_page); // Set the new layout for this activity
-        // Additional setup for the new activity
-    }
-}
-
-*/
-//The above is the template for a new page. Do not delete it until we turn the project in.
 package com.example.csci4320proj;
 
 import androidx.appcompat.app.AppCompatActivity;
